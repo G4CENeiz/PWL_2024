a7cf634e56a6d6f0595e9c25f48f06f5b6595015
Baihaqi <baihaqilearning+github@gmail.com>
2024-02-23 15:31:10 +0700

    feat: add route per the practicum steps

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Welcome';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+}); 
+
+Route::get('/about', function () {
+    return '2241720145 - Muhammad Baihaqi Aulia Asyari';
+}); 
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya ' . $name;
+}); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' komentar ke-: ' . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Article Page with ID ' . $id;
 });
